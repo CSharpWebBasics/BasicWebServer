a300c4a78a32b96de4add0a70a37faaf75049741
BiserBorisov <butoff@abv.bg>
2018-05-19 17:32:54 +0300

    Issue #2 Header and HeaderCollection added

---
+++ BasicWebServer/Server/HTTP/Contracts/IHttpHeaderCollection.cs
@@ -0,0 +1,15 @@
+﻿
+
+using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Server.Handlers.Contracts
+{
+    public interface IHttpHeaderCollection
+    {
+        void AddHeader(HttpHeader header);
+
+        bool ContainsKey(string key);
+
+        HttpHeader GetHeader(string key);
+    }
+}
