ab9b2e0ae2d189f2b6a858d87f1de495e4f93cb5
BiserBorisov <butoff@abv.bg>
2018-05-19 18:18:35 +0300

    Not found response added

---
+++ BasicWebServer/Server/HTTP/Response/NotFoundResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicWebServer.Server.HTTP.Response
+{
+    public class NotFoundResponse : HttpResponse
+    {
+        public NotFoundResponse()
+        {
+            this.StatusCode = HttpStatusCode.NotFound;
+        }
+    }
+}
