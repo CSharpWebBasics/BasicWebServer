f3054515c7dfb0968f8cbc1e506f807b35fb43ea
BiserBorisov <butoff@abv.bg>
2018-05-19 18:34:08 +0300

    Issue #7 completed

---
+++ BasicWebServer/Server/Handlers/Contracts/IRequestHandler.cs
@@ -0,0 +1,10 @@
+﻿
+using BasicWebServer.Server.HTTP.Contracts;
+
+namespace BasicWebServer.Server.Handlers.Contracts
+{
+    public interface IRequestHandler
+    {
+        IHttpResponse Handle(IHttpRequest httpRequest);
+    }
+}
